f73f0398bba139bff6f9a8e8ca4cc8b10cdce251
Ana Silva <ananunesdasilva@gmail.com>
2017-11-15 12:21:35 +0000

    updated js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,12 +5,4 @@
 //= require_tree .
 $('.datepicker').datepicker({format: 'yyyy-mm-dd'});
 
-// search with icons
 
-const icons = document.getElementsByClassName("banner-icons-type");
-for (let icon of icons) {
-  icon.addEventListener("click", (event) => {
-  console.log(event);
-  console.log(event.currentTarget);
-});
-};
