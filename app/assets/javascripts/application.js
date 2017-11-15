502407f57731d63e1d8c31b21770fbcf0a25912e
Ana Silva <ananunesdasilva@gmail.com>
2017-11-15 15:21:46 +0000

    searches are directing to the results - added javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,6 +5,15 @@
 //= require underscore
 //= require gmaps/google
 //= require_tree .
-$('.datepicker').datepicker({format: 'yyyy-mm-dd'});
+
+$(document).ready(function() {
+
+  if (window.location.search.match(/type|query/)) {
+    $('html, body').scrollTop($('#results').offset().top - 140);
+  }
+
+  $('.datepicker').datepicker({format: 'yyyy-mm-dd'});
+
+})
 
 
