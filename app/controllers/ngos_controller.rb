7cf14b49fbe3b971d122097eed8bff38a51377ab
Ana Silva <ananunesdasilva@gmail.com>
2017-11-16 11:25:06 +0000

    added new and create to the NGOs controller

--- app/controllers/ngos_controller.rb
+++ app/controllers/ngos_controller.rb
@@ -1,4 +1,21 @@
 class NgosController < ApplicationController
+  def new
+    @user = current_user
+    @ngo = Ngo.new
+  end
+
+  def create
+    @user = current_user
+    @ngo = NGO.new(ngo_params)
+    @ngo.user = current_user
+
+    if @ngo.save
+      redirect_to controller: "users", action: "show", id: "#{current_user.id}"
+    else
+      render :new
+    end
+  end
+
   def index
     if params[:query]
       @ngos = Ngo.search_by_name_and_address(params[:query])
@@ -14,9 +31,15 @@ class NgosController < ApplicationController
       # marker.picture ({ url: "https://i.imgur.com/B0Q6ghF.png"})
     end
   end
- 
+
   def show
     @ngo = Ngo.find(params[:id])
     @donation = Donation.new
+  end
+
+  private
+
+  def ngo_params
+    params.require(:ngo).permit(:name, :address, :lat, :lng)
   end
 end
